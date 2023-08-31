0f5a2e59036e819f8487d40c247ce8a5174f13f9
Daniel <danielsanchezpinazo@gmail.com>
2023-08-31 15:53:17 +0200

    Fin Sección 17

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './dynamic-page.component.html',
+  styles: [
+  ]
+})
+export class DynamicPageComponent {
+
+  constructor( private fb: FormBuilder ) { }
+
+  public myForm: FormGroup = this.fb.group({
+
+    name: ["", [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.fb.array([
+      [ "GTA IV", Validators.required ],
+      [ "Red Dead Redemption", Validators.required ],
+      [ "The Last of Us", Validators.required ],
+    ])
+  });
+
+  public newFavorite: FormControl = new FormControl( "", Validators.required );
+
+  onSubmit(): void {
+
+    if ( this.myForm.invalid ) {
+
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    (this.myForm.controls["getFavoriteGames"] as FormArray) = this.fb.array( [] );
+    this.myForm.reset();
+  }
+
+  get getFavoriteGames() {
+
+    return this.myForm.get("favoriteGames") as FormArray;
+  }
+
+  isValidField( field: string ): boolean | null {
+
+    return this.myForm.controls[field].errors
+     && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+
+    return formArray.controls[index].errors
+    && formArray.controls[index].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[ field ] ) return null;
+
+    const errors = this.myForm.controls[ field ].errors || {};
+
+    // console.log({errors});
+
+    for (const key of Object.keys( errors )) {
+
+      switch (key) {
+
+        case "required":
+          return "Este campo es requerido.";
+
+        case 'minlength':
+          return `El nombre del producto debe tener ${ errors['minlength'].requiredLength } dígitos mínimo.`;
+      }
+    }
+
+    return null;
+
+  }
+
+  onDeleteFavorite( index:number ) {
+
+    this.getFavoriteGames.removeAt( index );
+  }
+
+  addToFavorites(): void {
+
+    if ( this.newFavorite.invalid ) return;
+
+    const newGame = this.newFavorite.value;
+
+    // this.getFavoriteGames.push( new FormControl( newGame, Validators.required ))
+
+    this.getFavoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+  }
+
+}
